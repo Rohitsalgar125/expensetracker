3d7f240a291a7aa375a16955488c7fd4a0f15aa6
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-01-16 20:43:54 +0530

    navigation

--- src/hooks/hooks.jsx
+++ src/hooks/hooks.jsx
@@ -1,14 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
 import fetchData from "../helper";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+
 
 const useLoginMutation = () => {
+  const navigate = useNavigate()
   return useMutation({
     mutationFn: (data) =>
       fetchData("https://exptrack-kziy.onrender.com/login", "POST", data),
     onSuccess: (data) => {
       localStorage.setItem("userData", JSON.stringify(data));
       toast.success(data.response);
+      navigate('/dashboard')
     },
   });
 };
